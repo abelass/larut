6f9f5d57cc25637abfc4076332e27346d96ce164
abelass <websolutions@mychacra.net>
2013-02-23 17:51:59 +0100

    possibilité de choisir les images et leurs modes d'affichage

---
+++ squelettes/lang/local_fr.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // G
+    'galerie_photos' => 'Galerie photos',
+    'une_photo' => 'Une Photo', 
+    
+    //S
+    'somme_produit' => 'Suma'
+);
+
+?>
