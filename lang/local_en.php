f13a05275fe41de07f131b6616c391a5357205cd
Rainer Müller <rainer@websimple.be>
2013-07-05 17:36:57 +0200

    pluginiser le sqeulette

---
+++ lang/local_en.php
@@ -0,0 +1,31 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+
+    // G
+    'galerie_photos' => 'Galerie photos',
+    'une_photo' => 'Une Photo', 
+    
+    //L
+    'label_a_lire_sur' => 'Read on',      
+    'label_inscrit_newsletter' => 'You are subscribed to the newsletter from ',  
+    'lire_la_suite'=>'Read More',
+    'label_publie' => 'Published on:',        
+    
+    //N
+    'news' => 'Just posted',   
+    
+    //S
+    'somme_produit' => 'Suma',
+    
+    //V
+    'voir_plus' => 'See more',        
+);
+
+?>
