079f040d8557641ace595ce589fb20b4a4f8d2a0
Rainer Müller <rainer@websimple.be>
2013-08-30 09:30:56 +0200

    customusation pour l'envoi du mail pour le client

--- larut_fonctions.php
+++ larut_fonctions.php
@@ -81,3 +81,35 @@ function inc_donnees_reservations_details($id_reservations_detail,$set) {
        }
     return $set;
 }
+
+
+// Surcharge de notifications_reservation_client_dist
+function notifications_reservation_client($quoi,$id_reservation, $options) {
+    include_spip('inc/config');
+    $config = lire_config('reservation_evenement');
+
+    $envoyer_mail = charger_fonction('envoyer_mail','inc');
+    
+    $options['id_reservation']=$id_reservation;  
+    $options['qui']='client';     
+    $subject=_T('reservation:votre_reservation_sur',array('nom'=>$GLOBALS['meta']['nom_site']));
+
+    $message=recuperer_fond('notifications/contenu_reservation_mail',$options);
+     
+    //
+    // Envoyer les emails
+    //
+    //
+    //
+    $nom='TissusBruxelles.pdf';
+    $fichier=realpath(find_in_path('docs/'.$nom));
+    $o=array('html'=>$message);
+    if($options['statut']=='accepte')$o['pieces_jointes'] = array(
+               array('chemin' => $fichier,
+               'nom' => $nom,
+               'encodage' => 'base64',
+               'mime' => 'application/pdf')
+               );
+
+    $envoyer_mail($options['email'],$subject,$o);
+}
