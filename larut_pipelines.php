af67fca44a01ec1bb3cdf94556e5a36fa28a6966
unknown <mullrai@D02DI1146441.net1.cec.eu.int>
2014-04-17 13:49:52 +0200

    retour en arrière en attendant mune décision sur comment faire

--- larut_pipelines.php
+++ larut_pipelines.php
@@ -3,10 +3,12 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function larut_recuperer_fond($flux){
     $fond=$flux['args']['fond'];
-    if ($fond == 'inclure/champs_listes'){
+    //if ($fond == 'inclure/champs_listes'){
+    if ($fond == 'formulaires/reservation'){	
         $champs_amis=recuperer_fond('formulaires/inc-reservation_amies',$flux['data']);
         $login = pipeline('social_login_links', '');
-        $flux['data']['texte'] .= $champs_amis;
+		$flux['data']['texte'] = str_replace('<!--extra-->', '<!--extra-->' .  $champs_amis, $flux['data']['texte']);
+        //$flux['data']['texte'] .= $champs_amis;
     }
     
     return $flux;
