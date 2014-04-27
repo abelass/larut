bdc88f14c03801380addb13cec2b6790615788a7
Rainer <rainer@websimple.be>
2014-04-27 14:16:25 +0200

    début formulaire inscriptipn newsletter

--- larut_pipelines.php
+++ larut_pipelines.php
@@ -3,10 +3,20 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function larut_recuperer_fond($flux){
     $fond=$flux['args']['fond'];
+	
+	//Inclure les champs extras dans le formulaire reservation
     if ($fond == 'inclure/champs_listes'){
         $champs_amis=recuperer_fond('formulaires/inc-reservation_amies',$flux['data']);
-       // $login = pipeline('social_login_links', '');
         $flux['data']['texte'] .= $champs_amis;
+    }
+	//Selecteur des mailignlists
+    if ($fond == 'formulaires/newsletter_subscribe'){
+    	$flux['data']['status']='open';
+		$contexte=$flux['data']['contexte'];
+		$contexte['status']='open';
+		$contexte['name']='listes';		
+        $listes=recuperer_fond('formulaires/inc-check-subscribinglists',$contexte);
+        $flux['data']['texte'] = str_replace('<!--extra-->',$listes. '<!--extra-->',$flux['data']['texte']);
     }
     
     return $flux;
@@ -14,12 +24,42 @@ function larut_recuperer_fond($flux){
 
 function larut_formulaire_charger($flux){
     $form = $flux['args']['form'];
+	//Formulaire reservations
     if ($form == 'reservation'){
-        $flux['data']['email_amies'] .= "";
+        $flux['data']['email_amies'] = "";
     }
+	
+    if ($form == 'newsletter_subscribe'){
+    	include_spip('inc/mailsubscribers');
+		if (isset($GLOBALS['visiteur_session']['email']))
+			$email = $GLOBALS['visiteur_session']['email'];
+		elseif (isset($GLOBALS['visiteur_session']['session_email']))
+			$email = $GLOBALS['visiteur_session']['session_email'];		
+		If($email){
+			$listes=sql_getfetsel('listes','spip_mailsubscribers','email='.sql_quote($email));
+			$listes=explode(',',$listes);
+			}
+		$flux['data']['listes'] = $listes;
+		$flux['data']['choix_listes'] = mailsubscribers_listes();
+    }
+    
     
     return $flux;
 }
+
+function larut_formulaire_verifier($flux){
+    $form = $flux['args']['form'];
+	//Formulaire reservations
+	
+    if ($form == 'newsletter_subscribe'){
+
+		$flux['data']['choix_listes'] = mailsubscribers_listes();
+    }
+    
+    
+    return $flux;
+}
+
 
 function larut_formulaire_traiter($flux){
     $form = $flux['args']['form'];
