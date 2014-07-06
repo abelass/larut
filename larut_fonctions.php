9d19cda1e50d8972663deb7ec1290c9cea61ae0b
Rainer <rainer@websimple.be>
2014-07-06 17:37:55 +0200

    enlever les logs

--- larut_fonctions.php
+++ larut_fonctions.php
@@ -21,7 +21,7 @@ function notifications_reservation_client($quoi,$id_reservation, $options) {
 
     if(isset($GLOBALS[$var_reservation][$chaine_statut]))$subject=_T('reservation:'.$chaine_statut,array('nom'=>$GLOBALS['meta']['nom_site']));  
 
-    spip_log($GLOBALS[$var_reservation],'teste');	
+    	
 
     $message=recuperer_fond('notifications/contenu_reservation_mail',$options);
     $o=array('html'=>$message);
@@ -37,9 +37,19 @@ function notifications_reservation_client($quoi,$id_reservation, $options) {
     $id_article=array();
     
     while($data=sql_fetch($sql)) $id_article[] = $data['id_article'];
+	
 
     //Les documents attachés à ces articles quin on un mot clés attaché correpondant au statut de la réservation
-    $sql=sql_select('*','spip_mots LEFT JOIN spip_mots_liens USING(id_mot) LEFT JOIN spip_documents ON spip_mots_liens.id_objet=spip_documents.id_document LEFT JOIN spip_documents_liens USING (id_document) LEFT JOIN spip_types_documents USING(extension)','spip_mots.titre ='.sql_quote($options['statut']).' AND spip_documents_liens.id_objet IN ('.implode(',',$id_article).') AND spip_documents_liens.objet="article"');
+    if(count($id_article)>0){
+    	$sql=sql_select('*',
+    		'spip_mots LEFT JOIN spip_mots_liens USING(id_mot)
+			 LEFT JOIN spip_documents ON spip_mots_liens.id_objet=spip_documents.id_document 
+			 LEFT JOIN spip_documents_liens USING (id_document) 
+			 LEFT JOIN spip_types_documents USING(extension)',
+    		'spip_mots.titre ='.sql_quote($options['statut']).' 
+    		AND spip_documents_liens.id_objet IN ('.implode(',',$id_article).') 
+    		AND spip_documents_liens.objet="article"'
+			);
     $id_document=array();
     while($doc=sql_fetch($sql)){
         $fichier=$doc['fichier'];
@@ -48,7 +58,7 @@ function notifications_reservation_client($quoi,$id_reservation, $options) {
         $chemin=realpath(_DIR_IMG.$fichier);
         $o['pieces_jointes'][] = array('chemin' => $chemin,'nom' => $nom,'encodage' => 'base64','mime' => $doc['mime_type']) ;
         }
-
+	}
     $envoyer_mail($options['email'],$subject,$o);
     
         if ($archiver = charger_fonction('archiver_notification','inc',true)) {
