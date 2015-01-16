1727aedb4f54e9b2cfbb94b950c1e7f18ed4a305
Rainer <rainer@websimple.be>
2015-01-16 10:30:33 +0100

    code inutil

--- larut_pipelines.php
+++ larut_pipelines.php
@@ -9,15 +9,7 @@ function larut_recuperer_fond($flux){
         $champs_amis=recuperer_fond('formulaires/inc-reservation_amies',$flux['data']);
         $flux['data']['texte'] .= $champs_amis;
     }
-	//Selecteur des mailignlists
-    /*if ($fond == 'formulaires/newsletter_subscribe'){
-    	$flux['data']['status']='open';
-		$contexte=$flux['data']['contexte'];
-		$contexte['status']='open';
-		$contexte['name']='listes';		
-        $listes=recuperer_fond('formulaires/inc-check-subscribinglists',$contexte);
-        $flux['data']['texte'] = str_replace('<!--extra-->',$listes. '<!--extra-->',$flux['data']['texte']);
-    }-*/    
+ 
     return $flux;
 }
 
@@ -28,39 +20,12 @@ function larut_formulaire_charger($flux){
         $flux['data']['email_amies'] = "";
     }
 	
-    /*if ($form == 'newsletter_subscribe'){
-    	include_spip('inc/mailsubscribers');
-		if (isset($GLOBALS['visiteur_session']['email']))
-			$email = $GLOBALS['visiteur_session']['email'];
-		elseif (isset($GLOBALS['visiteur_session']['session_email']))
-			$email = $GLOBALS['visiteur_session']['session_email'];		
-		If($email){
-			$listes=sql_getfetsel('listes','spip_mailsubscribers','email='.sql_quote($email));
-			$listes=explode(',',$listes);
-			}
-		$flux['data']['listes'] = $listes;
-		$flux['data']['choix_listes'] = mailsubscribers_listes();
-    }*/
+
     
     
     return $flux;
 }
 
-/*
- * deactivated untill i know what it is about
- * function larut_formulaire_verifier($flux){
-    $form = $flux['args']['form'];
-	//Formulaire reservations
-	
-    if ($form == 'newsletter_subscribe'){
-    	include_spip('inc/mailsubscribers');
-		
-		if(function_exists('mailsubscribers_listes'))$flux['data']['choix_listes'] = mailsubscribers_listes();
-    }
-    
-    
-    return $flux;
-}*/
 
 
 function larut_formulaire_traiter($flux){
