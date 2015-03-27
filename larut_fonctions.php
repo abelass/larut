a0b18c59855cea7fadcc176d0c001e13ed80074a
Rainer <rainer@websimple.be>
2015-03-27 19:19:02 +0100

    afficher les langues multilingues

--- larut_fonctions.php
+++ larut_fonctions.php
@@ -88,3 +88,28 @@ if(!function_exists('statuts_complet')){
 		return $statuts;
 	}	
 } 
+
+/**
+ * Retourne les langues de l'article
+ * @param string $id_article
+ * @param mixed $separateur Ce qui entoure le résultat
+ **/
+ 
+ function article_langues($id_article,$separateur=''){
+ 	if(is_array($separateur)){
+ 		list($sep,$sep_fin)=$separateur;
+		$sep=$separateur[0];
+		$sep_fin=$separateur[1];
+ 	}
+	else $sep=$separateur;
+	
+	if(!$sql=sql_select('lang','spip_articles','id_trad!=0 AND id_trad='.$id_article))
+		$sql=sql_select('lang','spip_articles','id_trad!=0 AND id_article='.$id_article);
+	
+	$langues='';
+	while($data=sql_fetch($sql)){
+		$langues=concat($langues,$sep.$data['lang'].$sep_fin);
+	}
+ 	
+	return $langues;
+ }
