69fd64716fef27d83af8ae5d291b207e06521cf4
Rainer <rainer@websimple.be>
2016-05-07 10:45:58 +0200

    permettre l'inscription quan événement a commencé

--- larut_pipelines.php
+++ larut_pipelines.php
@@ -2,34 +2,38 @@
 if (! defined ( '_ECRIRE_INC_VERSION' ))
 	return;
 function larut_recuperer_fond($flux) {
-	$fond = $flux ['args'] ['fond'];
+	$fond = $flux['args']['fond'];
 	
 	// Inclure les champs extras dans le formulaire reservation
 	if ($fond == 'inclure/champs_listes') {
-		$champs_amis = recuperer_fond ( 'formulaires/inc-reservation_amies', $flux ['data'] );
-		$flux ['data'] ['texte'] .= $champs_amis;
+		$champs_amis = recuperer_fond ( 'formulaires/inc-reservation_amies', $flux['data'] );
+		$flux['data']['texte'] .= $champs_amis;
 	}
 	
 	return $flux;
 }
 function larut_formulaire_charger($flux) {
-	$form = $flux ['args'] ['form'];
+	$form = $flux['args']['form'];
 	// Formulaire reservations
 	if ($form == 'reservation') {
-		$flux ['data'] ['email_amies'] = "";
+		$flux['data']['email_amies'] = "";
+		if (!isset($flux['data']['evenements']) OR (isset($flux['data']['evenements']) AND count($flux['data']['evenements'] == 0)) AND $id_evenement = $flux['data']['id_evenement']) {
+			$evenement = sql_fetsel('*', 'spip_evenements', 'id_evenement_source IN (' . implode(',', $id_evenement) . ') AND date_fin>NOW()', '', 'date_fin DESC');
+			$flux['data']['evenements'][$evenement['id_evenement_source']] = $evenement;
+		}
 	}
 	
 	if ($form == 'editer_evenement') {
 		// Charger la valeur par défaut, hérité de l'article
-		if (! $flux ['data'] ['multilingue']) {
-			list ( $objet, $id_article ) = explode ( '|', $flux ['data'] ['parents_id'] [0] );
-			$flux ['data'] ['multilingue'] = sql_getfetsel ( 'multilingue', 'spip_articles', 'id_article=' . $id_article );
+		if (! $flux['data']['multilingue']) {
+			list ( $objet, $id_article ) = explode ( '|', $flux['data']['parents_id'][0] );
+			$flux['data']['multilingue'] = sql_getfetsel ( 'multilingue', 'spip_articles', 'id_article=' . $id_article );
 		}
 	}
 	return $flux;
 }
 function larut_formulaire_traiter($flux) {
-	$form = $flux ['args'] ['form'];
+	$form = $flux['args']['form'];
 	if ($form == 'reservation') {
 		if ($emails_amies = _request ( 'email_amies' )) {
 			$notifications = charger_fonction ( 'notifications', 'inc', true );
@@ -39,7 +43,7 @@ function larut_formulaire_traiter($flux) {
 					'evenements' => _request ( 'evenements' ),
 					'nom' => _request ( 'nom' ) 
 			);
-			$notifications ( 'email_amies', $data ['id_reservation'], $options );
+			$notifications ( 'email_amies', $data['id_reservation'], $options );
 		}
 	}
 	
