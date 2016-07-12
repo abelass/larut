5831cd83fb1fcd5f6fbd65002842931e71983e0d
abelass <rainer@websimple.be>
2016-07-12 12:13:50 +0200

    changement de compte changer le messages si accepte_part

--- lang/larut_fr.php
+++ lang/larut_fr.php
@@ -1,47 +1,47 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// This is a SPIP language file -- Ceci est un fichier langue de SPIP
+if (! defined ( '_ECRIRE_INC_VERSION' ))
+	return;
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+$GLOBALS [$GLOBALS ['idx_lang']] = array(
+	// A
+	'acompte_paye' => 'acompte payé @montant@ €',
+	'ateliers_tous' => 'Tous le ateliers',
 
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-	//A
-	'ateliers_tous'=>'Tous le ateliers', 
+	// C
+	'cordialement' => 'Cordialement',
+	'commentaire' => 'Commentaire',
+	'complet' => 'Complet',
+	'cours' => 'cours',
 
-    //C
-    'cordialement'=>'Cordialement',        
-    'commentaire'=>'Commentaire',    
-    'complet'=>'Complet', 
-    'cours'=>'cours',         
-      
-    //G
-    'galerie'=>'Galerie d\'images',
-    'galeries'=>'Galeries  d\'images',    
-    
-    //E
-    'explication_label_email_amies'=>'adresse(s) email, séparées par virgule(s)',
-    'explication_mailinglists'=>'Vous seriez automatiquement inscrit à la mailinglist interne mais vous pouvez vous inscrire à d\'autres listes',    
-    
-    //F
-    'fermer'=>'fermer',
-    
-    //I
-    'inscription'=>'Inscription',
-    'inscription_newsletter'=>'Inscription Newsletter',  
-    'inscrire_liste_attente'=>'Choissisez un autre cours ou inscrivez vous dans la liste d\'attente',
+	// G
+	'galerie' => 'Galerie d\'images',
+	'galeries' => 'Galeries  d\'images',
+
+	// E
+	'explication_label_email_amies' => 'adresse(s) email, séparées par virgule(s)',
+	'explication_mailinglists' => 'Vous seriez automatiquement inscrit à la mailinglist interne mais vous pouvez vous inscrire à d\'autres listes',
+
+	// F
+	'fermer' => 'fermer',
+
+	// I
+	'inscription' => 'Inscription',
+	'inscription_newsletter' => 'Inscription Newsletter',
+	'inscrire_liste_attente' => 'Choissisez un autre cours ou inscrivez vous dans la liste d\'attente',
 
 	// L
-	'label_email_amies' => 'J\'aimerai partager cette information avec mes amies',	
+	'label_email_amies' => 'J\'aimerai partager cette information avec mes amies',
 	'label_horaire' => 'Horaire :',
-	'label_multilingue' => 'Cours multilingue :',		
-    'label_prix' => 'Prix :',   		
+	'label_multilingue' => 'Cours multilingue :',
+	'label_prix' => 'Prix :',
 	'larut_titre' => 'Larut',
-	'liens' => 'Liens',	
-	
-    
-    //M
-    'merci_de_votre_reservation_accepte'=>'<p>Votre paiement a bien été reçu et nous pouvons donc confirmer votre inscription à un atelier de couture avec Larut.</p>', 
-    'merci_de_votre_reservation_accepte_part'=>'<p>Votre paiement a bien été reçu et nous pouvons donc confirmer votre inscription à un atelier de couture avec Larut.</p>',
-    'merci_de_votre_reservation_accepte_2'=>'
+	'liens' => 'Liens',
+
+	// M
+	'merci_de_votre_reservation_accepte' => '<p>Votre paiement a bien été reçu et nous pouvons donc confirmer votre inscription à un atelier de couture avec Larut.</p>',
+	'merci_de_votre_reservation_accepte_part' => '<p>Votre paiement a bien été reçu et nous pouvons donc confirmer votre inscription à un atelier de couture avec Larut.</p>',
+	'merci_de_votre_reservation_accepte_2' => '
     <p>N’hésitez pas à me contacter si vous avez des questions.</p>
     <ul>
         <li>GSM 0495/102818</li>
@@ -49,46 +49,45 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     </ul>
     <p>Je vous envoie une liste d\'endroits ou vous pouvez acheter vos tissus à
 Bruxelles en pièce jointe.</p>
-    ',       
-    'merci_de_votre_reservation_attente_paiement'=>'<p>Merci de votre intérêt pour nos ateliers de couture.</p>
+    ',
+	'merci_de_votre_reservation_attente_paiement' => '<p>Merci de votre intérêt pour nos ateliers de couture.</p>
 
-<p>Votre inscription est bien enregistrée dans notre système mais elle ne sera confirmée qu’après réception du paiement sur le compte bancaire de la La Transformathèque asbl BE04 3631 4999 0231 (swift/bic : BBRU BE BB) avec la référence : @reference@. Lors de la réception du paiement un e-mail de confirmation vous sera envoyé avec les détails pour le premier cours.</p>
+<p>Votre inscription est bien enregistrée dans notre système mais elle ne sera confirmée qu’après réception du paiement sur le compte bancaire BE84377452749759 (swift/bic : BBRUBEBB) avec la référence : @reference@. Lors de la réception du paiement un e-mail de confirmation vous sera envoyé avec les détails pour le premier cours.</p>
 
 <p>Pour plus de détails, veuillez consulter nos  <a href="@url_cg@">conditions générales</a>.</p>
 
-<p>Je me réjouis de vous rencontrer bientôt pour partager la magie de la couture avec vous!</p>', 
-    'message_partager_information'=>'@nom@ veut partager avec vous l’information sur le cours pour le quel elle/lui s\'est inscrit/e :',  
-    'message_partager_informations'=>'@nom@ veut partager avec vous l’information sur les cours pour les quels elle/lui s\'est inscrit/e :',       
-    'modele_cours'=>'Resumé cours',
-    'modele_label_afficher_texte'=>'Afficher le texte',   
+<p>Je me réjouis de vous rencontrer bientôt pour partager la magie de la couture avec vous!</p>',
+	'message_partager_information' => '@nom@ veut partager avec vous l’information sur le cours pour le quel elle/lui s\'est inscrit/e :',
+	'message_partager_informations' => '@nom@ veut partager avec vous l’information sur les cours pour les quels elle/lui s\'est inscrit/e :',
+	'modele_cours' => 'Resumé cours',
+	'modele_label_afficher_texte' => 'Afficher le texte',
 
-	//N
-	'inscription_newsletter'=>'Inscription Newsletter',
-    
-    //P
-    'pas_cours_programmee'=>'Ce cours n\'est pas programmé actuellement<br/> Appuyez sur le bouton inscription pour voir l\'offre actuelle de cours.',
-    'places'=>'Places :',   
-    'places_disponibles'=>'Places disponibles :',
-    'probleme_confirmation'=>'Cependant il y a un problème avec les cours suivants :',
-    
-    //R
-    'remerciement'=>'Cordialement,',
-    
-    // S
-    'seance'=>'Séance',    
-    'soutien'=>'Avec le soutien de',     
-    'sujet_email_amies'=>'Partage d\'infos de @nom@',
-    
-    // T
-    'texte_statut_bancaire'=>'Je vous invite à réaliser le virement dans le compte de La Transformathèque asbl BE04 3631 4999 0231 (swift/bic: BBRU BE BB) avec la référence : chèque cadeau et votre nom.',       
-    'texte_statut_merci'=>'Merci de votre commande d\'un chèque cadeau de Larut.',  
-    'texte_statut_reception'=>'Lors de la réception du paiement (comptez une moyenne de 3 jours pour les paiement depuis la Belgique) vous recevrez un mail avec le chèque cadeau (électronique).  N’hésitez pas à me contacter si vous souhaitez une autre modalité de livraison pour le chèque.',       
-    'texte_statut_archive'=>'archivé',
-    
-    // V
-    'voir_inscriptions_tous'=>'S\'inscrire pour plusieurs cours',  
-    'voir_tous_cours'=>'Voir tous les cours',       
+	// N
+	'inscription_newsletter' => 'Inscription Newsletter',
 
+	// P
+	'pas_cours_programmee' => 'Ce cours n\'est pas programmé actuellement<br/> Appuyez sur le bouton inscription pour voir l\'offre actuelle de cours.',
+	'places' => 'Places :',
+	'places_disponibles' => 'Places disponibles :',
+	'probleme_confirmation' => 'Cependant il y a un problème avec les cours suivants :',
+
+	// R
+	'remerciement' => 'Cordialement,',
+
+	// S
+	'seance' => 'Séance',
+	'soutien' => 'Avec le soutien de',
+	'sujet_email_amies' => 'Partage d\'infos de @nom@',
+
+	// T
+	'texte_statut_bancaire' => 'Je vous invite à réaliser le virement dans le compte de La Transformathèque asbl BE04 3631 4999 0231 (swift/bic: BBRU BE BB) avec la référence : chèque cadeau et votre nom.',
+	'texte_statut_merci' => 'Merci de votre commande d\'un chèque cadeau de Larut.',
+	'texte_statut_reception' => 'Lors de la réception du paiement (comptez une moyenne de 3 jours pour les paiement depuis la Belgique) vous recevrez un mail avec le chèque cadeau (électronique).  N’hésitez pas à me contacter si vous souhaitez une autre modalité de livraison pour le chèque.',
+	'texte_statut_archive' => 'archivé',
+
+	// V
+	'voir_inscriptions_tous' => 'S\'inscrire pour plusieurs cours',
+	'voir_tous_cours' => 'Voir tous les cours'
 );
 
 ?>
--- lang/local_fr.php
+++ lang/local_fr.php
@@ -1,28 +1,22 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
-
-
-
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-
-
-    // G
-    'galerie_photos' => 'Galerie photos',
-    'une_photo' => 'Une Photo',
-
-	//I
-	'inscription_newsletters'=>'avec l’inscription à un/plusieurs ateliers de Larut vous êtes automatiquement inscrit(e) à la liste d’envoi générale de Larut (Désinscription possible à tout moment). Mais vous pouvez choisir aussi de vous inscrire à :',
-
-    //L
-    'label_a_lire_sur' => 'A lire sur',
-    'label_enregistrer' => 'J\'aimerai que le site se souvienne de moi',
-    'label_inscrit_newsletter' => 'Vous êtes inscrit pour recevoir des nouvelles par mail de',
-    'lire_la_suite'=>'Lire la suite',
-    'label_publie' => 'Publié le :',
-
-	//M
+// This is a SPIP language file -- Ceci est un fichier langue de SPIP
+$GLOBALS [$GLOBALS ['idx_lang']] = array(
+	
+	// G
+	'galerie_photos' => 'Galerie photos',
+	'une_photo' => 'Une Photo',
+	
+	// I
+	'inscription_newsletters' => 'avec l’inscription à un/plusieurs ateliers de Larut vous êtes automatiquement inscrit(e) à la liste d’envoi générale de Larut (Désinscription possible à tout moment). Mais vous pouvez choisir aussi de vous inscrire à :',
+	
+	// L
+	'label_a_lire_sur' => 'A lire sur',
+	'label_enregistrer' => 'J\'aimerai que le site se souvienne de moi',
+	'label_inscrit_newsletter' => 'Vous êtes inscrit pour recevoir des nouvelles par mail de',
+	'lire_la_suite' => 'Lire la suite',
+	'label_publie' => 'Publié le :',
+	
+	// M
 	'message_evenement_cloture' => '<p>Merci d’avoir participé aux ateliers de couture de Larut. Ça a été un plaisir de vous accompagner dans la découverte de la magie de la couture ! J’aimerai vous animer à coudre régulièrement de votre côté pour ne pas oublier tout ça que vous venez d’apprendre et/ou a participer aux autres ateliers de couture programmés. Si l’expérience vous a plu je vous invite à en parler autour de vous ;-)</p>
 
 <p>Si vous êtes sur Facebook :</p>
@@ -40,26 +34,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 <p>Bonne couture ! :-)</p>
 ',
-
-  //N
-    'news' => 'Nouveau sur ce site',
-
-  //R
-    'reservation_enregistre'=>'Votre Inscription a bien été enregistré.<br/> Vous recevrez un email de confirmation avec les détails du paiement. Si aucun mail ne vous est parvenu, verifiez dans votre dossier spam. En cas de problème, envoyez moi un email <a href="mailto:contact@larut.eu">contact@larut.eu</a>, <br/> Merci',
-
-  //S
-  'somme_produit' => 'Suma',
+	
+	// N
+	'news' => 'Nouveau sur ce site',
+	
+	// R
+	'reservation_enregistre' => 'Votre Inscription a bien été enregistré.<br/> Vous recevrez un email de confirmation avec les détails du paiement. Si aucun mail ne vous est parvenu, verifiez dans votre dossier spam. En cas de problème, envoyez moi un email <a href="mailto:contact@larut.eu">contact@larut.eu</a>, <br/> Merci',
+	
+	// S
+	'somme_produit' => 'Suma',
 	'sujet_une_reservation_accepte' => 'Confirmation de participation @ @nom@',
 	'sujet_une_reservation_accepte_part' => 'Confirmation acompte reçu @ @nom@',
 	'sujet_votre_reservation_accepte' => '@nom@ : confirmation de votre participation',
 	'sujet_votre_reservation_accepte_part' => '@nom@ : confirmation partielle de votre participation',
 	'sujet_votre_reservation_cloture' => '@nom@ : clôture du cours',
-
-	//T
-	'telephone'=>'Téléphone',
-
-  //V
-  'voir_plus' => 'Voir d\'autres',
+	
+	// T
+	'telephone' => 'Téléphone',
+	
+	// V
+	'voir_plus' => 'Voir d\'autres' 
 );
 
 ?>
