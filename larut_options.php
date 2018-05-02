af1727619fdfbfc0aa55040adc8e4025d10b6176
abelass <rainer@websimple.be>
2018-05-02 11:44:50 +0200

    eviter une erreur avec multidomaines

---
+++ larut_options.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Options utiles au plugin Message personnalisé
+ *
+ * @plugin     Larut
+ * @copyright  2013 -2018
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Message_personnalise\Options
+ */
+
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
+
+define('_MULTIDOMAINE_RACINE', 0);
